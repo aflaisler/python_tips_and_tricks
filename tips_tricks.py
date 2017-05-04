f8880990bccceee67badba341a0ae59e90a7cf20
Aymeric Flaisler <aflaisler@gmail.com>
2017-05-03 20:45:45 -0700

    c

--- tips_tricks.py
+++ tips_tricks.py
@@ -172,12 +172,12 @@ from timeit import Timer
 
 # Make a list of functions to time.
 # callables without args...
-funcs = [< func_1 > , < func_2 > ]
+funcs = [ < func_1 > , < func_2 > ]
 tests = [(test_func.__name__, test_func) for test_func in funcs]
 
 for name, test in tests:
     # We have to do this because Timer takes a callable as an arg.
-    t = Timer(lambda: test( < parameter > ))
+    t = Timer(lambda: test(< parameter > ))
     print "Completed {name} in {time} seconds.".format(name=name,
                                                        time=t.timeit(1))
 
@@ -204,3 +204,4 @@ python - m memory_profiler example.py
 # to plot memory usage
 mprof run < exec.py >
 mprof plot
+#
